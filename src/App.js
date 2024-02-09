16522f578433ad0826093ed33a41845c93165a5e
wcrespo <wellingtoncrespo2001@gmail.com>
2024-02-08 23:57:29 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import './css/homePage.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div>
+            <img src="https://www.spacejam.com/1996/img/p-jamlogo.gif" alt="" />
+        </div>
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div>
+            <img src="https://www.spacejam.com/1996/img/p-jamlogo.gif" alt="" />
+        </div>
+        <div>
+            <img src="" alt="" />
+        </div>
+        <div>
+            <img src="" alt="" />
+        </div>
+        <div>
+            <img src="" alt="" />
+        </div>
+        <div>
+            <img src="" alt="" />
+        </div>
+        <div>
+            <img src="" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Home
